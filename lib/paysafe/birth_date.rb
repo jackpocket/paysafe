32879c1e13de8322a9123e44dc9b7fb48b60c42a
Javier Julio <jjfutbol@gmail.com>
2020-01-24 16:58:36 -0500

    Add date method that returns values as a Date object

--- lib/paysafe/birth_date.rb
+++ lib/paysafe/birth_date.rb
@@ -3,5 +3,9 @@ require 'paysafe/result'
 module Paysafe
   class BirthDate < Result
     attributes :year, :month, :day
+
+    def date
+      @date ||= Date.new(year, month, day)
+    end
   end
 end
