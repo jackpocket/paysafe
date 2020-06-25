a355c4a3c5f925949d63bda4a1888f4d6313d983
Javier Julio <jjfutbol@gmail.com>
2020-06-25 14:12:19 -0400

    Add a create verification failed test case

--- test/card_payments_api/verifications_test.rb
+++ test/card_payments_api/verifications_test.rb
@@ -42,6 +42,31 @@ class CardPaymentsApiVerificationsTest < Minitest::Test
     assert_equal '10014', verification.billing_details.zip
   end
 
+  def test_create_verification_failed_400
+    error = assert_raises(Paysafe::Error::BadRequest) do
+      VCR.use_cassette('card_payments_api/create_verification_failed_bad_request') do
+        authenticated_client.card_payments.create_verification(
+          merchant_ref_num: random_id,
+          card: {
+            card_num: 9999_9999_9999_9999.to_s,
+            card_expiry: {
+              month: 12,
+              year: 2050
+            },
+            cvv: 123
+          },
+          billing_details: {
+            country: 'US',
+            zip: '10014'
+          }
+        )
+      end
+    end
+
+    assert_equal '5068', error.code
+    assert_equal 'Either you submitted a request that is missing a mandatory field or the value of a field does not match the format expected. (Code 5068) Field Errors: The `card.cardNum` Luhn checksum failed.', error.message
+  end
+
   def test_get_verification
     verification = VCR.use_cassette('card_payments_api/get_verification') do
       result = authenticated_client.card_payments.create_verification(
