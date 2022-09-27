1bc297d77b2c34663f53ba1afef3cc7b5bd8716d
Javier Julio <jjfutbol@gmail.com>
2022-09-27 11:48:47 -0400

    Add SUT test for verification and address override
    We want to simulate a case for Paysafe.js that creates an SUT with only card info, no address. The create verification endpoint supports specifying an address that will override what's in the SUT if one is defined.

--- test/customer_vault_api/single_use_tokens_test.rb
+++ test/customer_vault_api/single_use_tokens_test.rb
@@ -80,6 +80,53 @@ class CustomerVaultApiSingleUseTokensTest < Minitest::Test
     assert result.cvv_match?
   end
 
+  def test_single_use_token_with_verification_and_address_override
+    result = VCR.use_cassette('customer_vault_api/single_use_token_with_verification_and_address') do
+      sut = sut_client.customer_vault.create_single_use_token(
+        card: {
+          card_num: '5200400000000009',
+          card_expiry: {
+            month: 12,
+            year: 2050
+          },
+          cvv: '123'
+        }
+      )
+
+      assert_match UUID_REGEX, sut.id
+      assert_match TOKEN_REGEX, sut.payment_token
+
+      authenticated_client.card_payments.create_verification(
+        merchant_ref_num: random_id,
+        card: {
+          paymentToken: sut.payment_token
+        },
+        billing_details: {
+          country: 'US',
+          zip: '10014'
+        }
+      )
+    end
+
+    assert_match UUID_REGEX, result.id
+    assert_match UUID_REGEX, result.merchant_ref_num
+    assert_equal 'MC', result.card.type
+    assert_equal 'master', result.card.brand
+    assert_equal '0009', result.card.last_digits
+    assert_equal 12, result.card.card_expiry.month
+    assert_equal 2050, result.card.card_expiry.year
+    assert_equal 'COMPLETED', result.status
+    assert Time.parse(result.txn_time)
+    assert_match AUTH_CODE_REGEX, result.auth_code
+    assert_equal 'US', result.billing_details.country
+    assert_equal '10014', result.billing_details.zip
+    assert_equal 'USD', result.currency_code
+    assert_equal 'MATCH', result.avs_response
+    assert result.avs_match?
+    assert_equal 'MATCH', result.cvv_verification
+    assert result.cvv_match?
+  end
+
   def test_single_use_token_with_profile_creation
     profile = VCR.use_cassette('customer_vault_api/single_use_token_with_profile_creation') do
       sut = sut_client.customer_vault.create_single_use_token(
