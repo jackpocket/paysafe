d9964a9f7867487dce669812174f0ce69307a63e
Javier Julio <jjfutbol@gmail.com>
2024-04-16 13:11:05 -0400

    Update code coverage in CI workflow
    This matches what we do in other repos for managing code coverage file and upload.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,6 @@ if ENV.fetch("COVERAGE", false)
   require "simplecov-cobertura"
   SimpleCov.start do
     add_filter %r{^/test/}
-    minimum_coverage 99
-    maximum_coverage_drop 0.2
     formatter SimpleCov::Formatter::CoberturaFormatter
   end
 end
